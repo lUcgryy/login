2924c25fbcb0b43be65ffc91b041d11175d96a43
Trần Trường Giang <92589367+lucasgreen138@users.noreply.github.com>
2022-06-04 22:49:50 +0700

    Initial commit

---
+++ router.js
@@ -0,0 +1,45 @@
+const e = require('express');
+var express = require('express');
+var router = express.Router();
+
+const credentials = {
+    username: 'admin',
+    password: 'admin'
+}
+//login user
+router.post('/login', (req, res) => {
+    username = req.body.username;
+    password = req.body.password;
+    if (username == credentials.username && password == credentials.password) {
+        req.session.user = username;
+        res.redirect('/admin');
+        //res.end("success");
+    } else {
+        res.end("Invalid credentials");
+    }
+});
+
+//admin
+router.get('/admin', (req, res) => {
+    if (req.session.user) {
+        res.render('admin', {
+            title: 'Admin',
+            user: req.session.user
+        });
+    } else {
+        res.send("You are not admin");
+    }
+});
+
+//logout
+router.get('/logout', (req, res) => {
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+            res.send("Error");
+        } else {
+            res.render('base', {title: "Logout", logout: "Logout Successful"});
+        }
+    });
+});
+module.exports = router;
